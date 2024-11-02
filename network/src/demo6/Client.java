9e035f1e0e45f016bd0f3e444ff9cc70c897296b
911-Zhou <3077313443@qq.com>
2024-11-03 00:23:39 +0800

    TcpEchoClient

--- network/src/demo6/Client.java
+++ network/src/demo6/Client.java
@@ -1,23 +1,53 @@
 package demo6;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
-    private Socket clientSocket = null;
+    private Socket serverSocket = null;
 
     public Client(String host, int port) throws IOException {
         InetAddress inetAddress = InetAddress.getByName(host);
-        clientSocket = new Socket(inetAddress, port);
+        serverSocket = new Socket(inetAddress, port);
     }
 
     public void start() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("客户端启动！");
-        while (true) {
+        try(InputStream inputStream = serverSocket.getInputStream();
+            OutputStream outputStream = serverSocket.getOutputStream()){
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            Scanner scannerIn = new Scanner(inputStream);
 
+            while (true) {
+                System.out.print("请输入请求->");
+                String request = scanner.next();
+
+                printWriter.println(request);
+                printWriter.flush();
+
+                if (!scannerIn.hasNext()) {
+                    System.out.println("服务器异常");
+                    break;
+                }
+
+                String response = scannerIn.nextLine();
+                System.out.println(response);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client("localhost",6666);
+        client.start();
     }
 }
